7ba6db53f78ba8ed954b85eae10b25b8b68f24d1
Alex D <alexarjundhillon@gmail.com>
2015-05-24 23:28:16 -0400

    My answers to Assin2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function which returns a list of functions
+## that allow an object to behave like a matrix
 
-## Write a short comment describing this function
+##cacheSolve checks for a cached solution, otherwise it calculates 
+##the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setinv <- function(inv) m <- inv
+    getinv <- function() m
+    
+    list(set = set, get = get, setinv = setinv,getinv = getinv)
+    
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    m <- x$getinv()
+    
+    if (!is.null(m)) {
+        
+        message("getting cached value")
+        return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data)
+    x$setinv(m)
+    m 
 }
